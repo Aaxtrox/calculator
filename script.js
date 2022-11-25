96d0b22c6a442856e880e794d5ac784d6dfd1a7b
Aaxtrox <pawelw@gmx.com>
2022-11-25 11:40:07 +0000

    new arrayes to store data and display 0 on reload

---
+++ script.js
@@ -0,0 +1,16 @@
+//grab id display inside container
+const display = document.querySelector('#display');
+
+//create empty array to store 1st number
+let firstNumber = [];
+//create empty array to store 2nd number
+let secondNumber = [];
+//create empty array to store operators
+let operators = [];
+//create empty array to store results
+let results = [];
+//display 0 on page load
+let displayValue = 0;
+
+//displayValue inside display
+display.textContent = displayValue;
